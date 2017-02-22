fec1e80c6266f5eb38b0156afdfdd8cccf293dbd
Sergio Cacho <sergi0.cach0@hotmail.com>
2017-02-23 00:50:34 +0100

    avance2

--- SecondActivity/app/src/main/java/com/example/carlosergio/secondactivity/MainActivity.java
+++ SecondActivity/app/src/main/java/com/example/carlosergio/secondactivity/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "com.example.secondapp.MESSAGE";
-    public final static String EXTRA_REPET = "com.example.secondapp.REPET";
+    private final static String EXTRA_MESSAGE = "com.example.secondapp.MESSAGE";
+    private final static String EXTRA_REPET = "com.example.secondapp.REPET";
 
     private Button mbtnActi;
     private EditText mMessage;
@@ -20,20 +21,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessage = (EditText) findViewById(R.id.edit_message);
+        mRepet = (EditText) findViewById(R.id.edit_repet);
         mbtnActi = (Button) findViewById(R.id.button);
+        mbtnActi.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
+
+                if(mMessage.getText().toString().trim().length() == 0){
+                    Toast.makeText(MainActivity.this, "The message is empty", Toast.LENGTH_SHORT).show();
+                }if((mRepet.getText().toString().trim().length() == 0)){
+                    Toast.makeText(MainActivity.this, "The number of repete is empty", Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent newIntent = new Intent(MainActivity.this, Message.class);
+                    newIntent.putExtra(EXTRA_MESSAGE, mMessage.getText().toString());
+                    newIntent.putExtra(EXTRA_REPET, mRepet.getText().toString());
+                    startActivity(newIntent);
+                }
+            }
+        });
     }
-
+    /*
     public void sendMessage(View view) {
         Intent intent = new Intent(this, Message.class);
         mMessage = (EditText) findViewById(R.id.edit_message);
         mRepet = (EditText) findViewById(R.id.edit_repet);
 
-        intent.putExtra(EXTRA_MESSAGE, mMessage.getText().toString());
+        //intent.putExtra(EXTRA_MESSAGE, mMessage.getText().toString());
         intent.putExtra(EXTRA_REPET, Integer.parseInt(mRepet.getText().toString()));
 
+
         startActivity(intent);
-    }
+    }*/
 
 
 }
--- SecondActivity/app/src/main/java/com/example/carlosergio/secondactivity/Message.java
+++ SecondActivity/app/src/main/java/com/example/carlosergio/secondactivity/Message.java
@@ -1,17 +1,44 @@
 package com.example.carlosergio.secondactivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Message extends AppCompatActivity {
+    private final static String EXTRA_MESSAGE = "com.example.secondapp.MESSAGE";
+    private final static String EXTRA_REPET = "com.example.secondapp.REPET";
+    private String mMessage;
+    private String nRept;
+    private String mMessageToShow="";
+    private TextView mtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+        mMessage = getIntent().getStringExtra(EXTRA_MESSAGE);//false porque es el valor antes de que le pasen el verdadero valor
+        nRept = getIntent().getStringExtra(EXTRA_REPET);
+        mtext = (TextView) findViewById(R.id.text_label);
+        for(int i = 0; i < Integer.parseInt(nRept); i++){
+            mMessageToShow += mMessage+" ";
+        }
+
+        mtext.setText(mMessageToShow);
+
+
     }
+    /*
+    @Override
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_CODE_CHAT){
 
+        }
+    }*/
 }
